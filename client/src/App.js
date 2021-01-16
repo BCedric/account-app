5fb43cf15ef2f765ee5fbd9aed8e058c8bf44c50
Cédric Bois <cedric.bois@uca.fr>
2021-01-16 12:12:58 +0100

    set moment locale

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { HashRouter as Router } from 'react-router-dom'
 import Boot from './components/Boot'
 import ServerMessageToast from './common/ServerMessageToast'
 
+import 'moment/locale/fr'
+
 function App() {
   return (
     <div className="App">
